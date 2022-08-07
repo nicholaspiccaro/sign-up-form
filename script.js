55749b41b36c76f90865a9dea1108478cb04c52c
Nicholas Piccaro <npiccaro10@gmail.com>
2022-08-07 13:29:45 -0600

    add .

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+var check = function() {
+    if (document.getElementById('password').value != document.getElementById('confirm-password').value) {
+      document.getElementById('match-text').innerHTML = "Passwords don't match";
+    }else{
+        document.getElementById('match-text').innerHTML = "";
+    }
+}
 
+var up = function() {
+    document.getElementById('match-text').innerHTML = 3;
+}
